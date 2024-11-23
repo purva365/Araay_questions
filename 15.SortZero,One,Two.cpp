fb5443fb8a82ace7e7f826523830fd9e777e9378
purva365 <purvaath5@gmail.com>
2024-11-23 21:45:07 +0530

    Update 15.SortZero,One,Two.cpp
    Signed-off-by: purva365 <purvaath5@gmail.com>

--- 15.SortZero,One,Two.cpp
+++ 15.SortZero,One,Two.cpp
@@ -52,3 +52,55 @@ int main() {
 
     return 0;
 }
+
+//second method
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(vector<int>&arr,int n)
+{
+int cnt0=0;
+int cnt1=0;
+int cnt2=0;
+for(int i=0;i<n;i++)
+{
+    if(arr[i]==0)
+    {
+        cnt0++;
+    }
+    else if(arr[i]==1)
+    {
+        cnt1++;
+    }
+    else{
+        cnt2++;
+    }
+}
+for(int i=0;i<cnt0;i++)
+{
+    arr[i]=0;
+}
+for(int i=cnt0;i<cnt0+cnt1;i++)
+{
+    arr[i]=1;
+}
+for(int i=cnt0+cnt1;i<n;i++)
+{
+    arr[i]=2;
+}
+};
+int main() {
+    
+  vector<int>arr={2,0,2,1,1,0};
+   int n=sizeof(arr)/sizeof(arr[0]);
+  sortArray(arr,n);
+   for(int i=0;i<n;i++)
+   {
+       cout<<arr[i]<<" ";
+   }
+   cout<<endl;
+  
+ 
+
+    return 0;
+}
